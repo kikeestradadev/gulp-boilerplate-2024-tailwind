4cd0e06cc7dc6f7672da1d826e17339230383269
kikeestrada <kikeestradadev@gmail.com>
2024-11-11 15:07:25 -0600

    adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,19 +13,28 @@ module.exports = {
 		'!./node_modules/**/*',  // Excluye node_modules
 	],
 	theme: {
-		extend: {
-			fontFamily: {
-				'montserrat': ['"Montserrat"', 'sans-serif'],
+		container: {
+			center: true, // Centra el contenedor
+			padding: '15px',
+			screens: {
+			DEFAULT: '100%', // Configura el ancho del contenedor para todas las pantallas
+			xl: '1600px', // Máximo ancho de 1600px para pantallas grandes
 			},
 		},
-		screens: {			
-			sm: 	'480px',
-			m: 		'640px',
-			l: 		'960px',
-			lg: 	'1280px',
-			xl: 	'1600px',
-			xxl: 	'1800px',
-			xxxl: 	'1920px',
+		extend: {
+		fontFamily: {
+				'sans': ['Montserrat', 'sans-serif'], 
+				'cascadia': ['CascadiaCode', 'sans-serif'] 
+			},
+		},
+		screens: {
+			sm: '480px',
+			m: '640px',
+			l: '960px',
+			lg: '1280px',
+			xl: '1600px',
+			xxl: '1800px',
+			xxxl: '1920px',
 		},
 	},
 	plugins: [],
